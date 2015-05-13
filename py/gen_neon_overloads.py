d4270877ebcef6ca038990876dc0733acd79a4eb
Kirill Kouzoubov <kirill888@gmail.com>
2015-05-13 21:31:37 +1000

    Now also generating methods that need templates
    Adding logic to generate templated overloads, for those methods that
    have same types of input parameters but different outputs while doing
    "the same thing". These are mostly loads and converts.

--- py/gen_neon_overloads.py
+++ py/gen_neon_overloads.py
@@ -5,31 +5,42 @@ from string import Template
 
 suffixes = set('f32 u8 s8 p8 u16 s16 p16 u32 s32 p32 u64 s64 p64'.split(' '))
 
+arg_names = 'a b c d e f g h j k'.split(' ')
+tpl_cpp_overload = Template('''static inline __attribute__((always_inline)) $result_type $cpp_name($args_decl){return $c_name($args);}''')
+tpl_cpp_template_decl=Template('template<typename R> R $cpp_name($args_decl);')
+tpl_cpp_templated_overload = Template('''template<> inline __attribute__((always_inline)) $result_type $cpp_name<$result_type>($args_decl){return $c_name($args);}''')
+
+
 def base_func_name(name):
     def unQ(s):
         if s[-1] == 'q': return s[:-1]
         return s
-    
+
     return '_'.join([unQ(s) for s in name.split('_') if not s in suffixes])
 
+def group_by_x(dd, get_key ):
+    ''' Given a list of items and a key generation function,
+        returns a dictionary of lists of items.
+    '''
+    ff = {}
+
+    def register(k,v):
+        if k in ff:
+            ff[k].append(v)
+        else:
+            ff[k] = [v]
+
+    for d in dd:
+        register( get_key(d), d)
+
+    return ff
 
 def group_by_name(dd):
-    ff = {}
-    
-    def register(n,v):
-        if n in ff:
-            ff[n].append(v)
-        else:
-            ff[n] = [v]
-            
-    for d in dd:
-        register(base_func_name(d.name), d)
-        
-    return ff
+    return group_by_x(dd, lambda d: base_func_name(d.name))
 
 def full_sig(f):
     return arg_sig(f) + ' -> ' + f.result_type
-    
+
 def arg_sig(f):
     return ' -> '.join(f.args)
 
@@ -51,16 +62,12 @@ def has_type_clash(ff):
 
 
 
-arg_names = 'a b c d e f g h j k'.split(' ')
-
-tpl_cpp_overload = Template('''static inline __attribute__((always_inline)) $result_type $cpp_name($args_decl){return $c_name($args);}''')
-
 
 def render_cpp(cpp_name, f, tpl):
     arg_types = f.args
     args = [(t,n) for t,n in zip(arg_types, arg_names)]
     args_decl = ', '.join([ '%s %s'%(t,n)  for t,n in args ])
-    args_list = ', '.join([n for _,n in args]) 
+    args_list = ', '.join([n for _,n in args])
 
     return tpl.substitute(result_type=f.result_type,
                           cpp_name=cpp_name,
@@ -72,15 +79,27 @@ def render_group(cpp_name, funcs, tpl):
     return '\n'.join([render_cpp(cpp_name, f, tpl) for f in funcs])
 
 
+def render_template_decl(cpp_name, ff, tpl = tpl_cpp_template_decl):
+    def render(g):
+        args_decl = ','.join(g[0].args)
+        return tpl.substitute(cpp_name=cpp_name, args_decl=args_decl)
+
+    gg = group_by_x(ff, arg_sig)
+    return '\n'.join(render(g) for g in gg.values())
+
+
 def load_hdr_data(json_file):
-    import json
+    try:
+        import simplejson as json
+    except ImportError:
+        import json
 
     def to_func(d):
         from collections import namedtuple
         Func = namedtuple('Func','name result_type args')
-    
+
         n,r,a = d[:3]
-        return Func(name=n, result_type=r, args=tuple(a)) 
+        return Func(name=n, result_type=r, args=tuple(a))
 
     with open(json_file,'r') as f:
         return [to_func(d) for d in json.load(f)]
@@ -94,9 +113,9 @@ if __name__ == '__main__':
 
     def dump(*objs):
         print(*objs, file=sys.stdout)
-        
+
     json_file = 'neon_header.json'
-    
+
     msg('Loading json file: %s'%(json_file))
     dd = load_hdr_data(json_file)
     msg('Loaded %d functions\n'%(len(dd) ))
@@ -105,18 +124,32 @@ if __name__ == '__main__':
     msg('Found %d function groups'%(len(ff)))
 
     clash_data = [ (k,has_overload_clash(v), has_type_clash(v)) for k,v in ff.iteritems() ]
-    f_simple = [ n for (n,c,_) in clash_data if c == False ]
 
-    msg('Of that %d groups can be trivially overloaded in C++'%(len(f_simple)))
+    f_simple = sorted( n for (n,c,_ ) in clash_data if c == False )
+    f_tpl    = sorted( n for (n,c,tc) in clash_data if c == True and tc == False )
+    f_bad    = sorted( n for (n,_,tc) in clash_data if tc == True )
+
+    msg('Of that\n  %4d groups can be trivially overloaded in C++'%(len(f_simple)))
+    msg('  %4d groups will be templated on return type'%(len(f_tpl)))
+
+    if len(f_bad) > 0:
+        msg('Found %d bad groups that have clashing types'%(len(f_bad)))
 
     dump('#include <arm_neon.h>')
-    
+
+    #do template declarations first
+    for n in f_tpl:
+        dump('\n\n//%s'%(n))
+        dump( render_template_decl(n, ff[n]) )
+
+    #do template specialisations
+    for n in f_tpl:
+        s = render_group(n, ff[n], tpl_cpp_templated_overload)
+        dump('\n\n//%s'%(n))
+        dump(s)
+
+    # do vanila overloads
     for n in f_simple:
         s = render_group(n, ff[n], tpl_cpp_overload)
         dump('\n\n//%s'%(n))
         dump(s)
-
-
-          
-
-    
